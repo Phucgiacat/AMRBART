6f0c7a640fc79f7bb2feaf4eb16c20593913d28a
Phucgiacat <132926094+Phucgiacat@users.noreply.github.com>
2026-01-26 02:13:34 +0700

    fix-bug-smatch

--- fine-tune/common/postprocessing.py
+++ fine-tune/common/postprocessing.py
@@ -15,10 +15,10 @@ BACKOFF = penman.Graph(
 )
 
 
-def fix_empty_concepts_in_amr_string(amr_string: str) -> str:
-    """Replace empty concepts like ( z / ) with placeholder."""
-    pattern = re.compile(r"\(\s*([a-z]\d+)\s*/\s*\)")
-    return pattern.sub(r"( \1 / amr-unknown )", amr_string)
+# def fix_empty_concepts_in_amr_string(amr_string: str) -> str:
+#     """Replace empty concepts like ( z / ) with placeholder."""
+#     pattern = re.compile(r"\(\s*([a-z]\d*)\s*/\s*\)")
+#     return pattern.sub(r"( \1 / amr-unknown )", amr_string)
 
 
 def token_processing(tok):
