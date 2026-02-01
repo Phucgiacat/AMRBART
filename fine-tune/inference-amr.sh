a2399d405f841b3518472942b478f7ff922c6011
Phucgiacat <132926094+Phucgiacat@users.noreply.github.com>
2026-02-01 21:05:54 +0700

    upload4Kaggle

--- fine-tune/inference-amr.sh
+++ fine-tune/inference-amr.sh
@@ -39,7 +39,7 @@ export CUDA_VISIBLE_DEVICES=0
 python3 main.py \
     --data_dir $DataPath \
     --task "text2amr" \
-    --test_file /kaggle/input/dataforparsing/test_code.jsonl \
+    --test_file /kaggle/input/dataforparsing/train.jsonl \
     --output_dir $OutputDir \
     --cache_dir $ModelCache \
     --data_cache_dir $DataCache \
